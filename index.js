e516bee7a5f408b8e08b3dd74a751f05af3f8f70
LuisAnge45 <88398632+LuisAnge45@users.noreply.github.com>
2026-02-02 22:26:52 -0400

    Viewer fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const url = './web/menu.pdf'; // Cambia esto por tu archivo
 
 // 1. Configurar el worker (usa el archivo de tu carpeta build)
-pdfjsLib.GlobalWorkerOptions.workerSrc = './build/pdf.worker.js';
+pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cafe-konh-cafe.netlify.app/build/pdf.worker.js';
 
 // 2. Cargar el documento
 const loadingTask = pdfjsLib.getDocument(url);
